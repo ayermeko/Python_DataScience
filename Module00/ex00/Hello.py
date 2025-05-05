41a1ccf82fb6fc3d86fdd212cb1f0b92c6c7c595
Alibi <yoooholly71@gmail.com>
2025-05-05 16:57:08 +0200

    push the changes

--- Module00/ex00/Hello.py
+++ Module00/ex00/Hello.py
@@ -3,12 +3,11 @@ ft_tuple = ("Hello", "toto")
 ft_set = {"Hello", "tutu"}
 ft_dict = {"Hello" : "titi!"}
 
-# your code here
 ft_list[1] = "World!"
 ft_tuple = ("Hello", "Czech Republic!")
 ft_set.remove("tutu")
 ft_set.add("Prague!")
-
+ft_dict["Hello"] = "42Prague!"
 
 print(ft_list)
 print(ft_tuple)
