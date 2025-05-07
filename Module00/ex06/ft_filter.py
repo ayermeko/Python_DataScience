68cac68b73140ebdf0e6b12567626a0af0b45187
Alibi Yermekov <ayermeko@c3r1s1.42prague.com>
2025-05-07 22:05:52 +0200

    my chnages

---
+++ Module00/ex06/ft_filter.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def ft_filter(function, iterable) -> list:
+    """Filters elements from an iterable based on a function."""
+    if function is None:
+        return (item for item in iterable if item)
+    
+    return (item for item in iterable if function(item))
+
+
+def main():
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    try:
+        argument_count = len(sys.argv)
+        assert argument_count <= 1, "more than no argument is provided"
+        print(f"My function:\t\t{list(ft_filter(lambda x: x % 2 == 0, numbers))}")
+        print(f"Orginal funciton:\t{list(filter(lambda x: x % 2 == 0, numbers))}")
+    except AssertionError as e:
+        print(f"{type(e).__name__}: {e}")
+        return
+
+if __name__ == "__main__":
+    main()
