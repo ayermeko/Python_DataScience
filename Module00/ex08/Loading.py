92c3dafe671c95d8b99d2fabf8f65060ca1ba148
Alibi Yermekov <ayermeko@c1r1s5.42prague.com>
2025-05-08 18:19:50 +0200

    push

--- Module00/ex08/Loading.py
+++ Module00/ex08/Loading.py
@@ -1,1 +1,8 @@
 
+def ft_tqdm(lst: range) -> None:
+    """
+    A simple implementation of a progress bar.
+    :param lst: The iterable to be processed.
+    :return value None
+    """
+    
---
+++ Module00/ex08/tester.py
@@ -0,0 +1,19 @@
+from time import sleep
+from tqdm import tqdm
+from Loading import ft_tqdm
+
+
+for elem in ft_tqdm(range(333)):
+    sleep(0.005)
+print()
+for elem in tqdm(range(333)):
+    sleep(0.005)
+print()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
