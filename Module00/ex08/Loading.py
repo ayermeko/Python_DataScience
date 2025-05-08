43621de896ccd15dbce2a3a83003d2d9d953eaa4
Alibi Yermekov <ayermeko@c1r1s5.42prague.com>
2025-05-08 19:10:08 +0200

    push

--- Module00/ex08/Loading.py
+++ Module00/ex08/Loading.py
@@ -1,8 +1,11 @@
 
 def ft_tqdm(lst: range) -> None:
-    """
-    A simple implementation of a progress bar.
-    :param lst: The iterable to be processed.
-    :return value None
-    """
+    total = len(lst)
+    for i, item in enumerate(lst, start=1):
+        percent = int((i / total) * 100)
+        bar_length = 50  # Width of the bar
+        filled = int(bar_length * i // total)
+        bar = '=' * (filled - 1) + '>' + ' ' * (bar_length - filled)
+        print(f'\r{percent:3}%|[{bar}]| {i}/{total}', end='', flush=True)
+        yield item
     
