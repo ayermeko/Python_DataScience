ddf4d1e6ab8eae1fe60dc86faad5ef03b06b6304
Alibi Yermekov <ayermeko@c2r4s4.42prague.com>
2025-05-09 19:58:03 +0200

    push it

---
+++ Module00/ex09/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Clean build files
+rm -rf dist/
+rm -rf *.egg-info/
+rm -rf venv/
+echo "Cleaned up build artifacts."
--- Module00/ex09/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="ft_package",
-    version="0.0.1",
-    author="ayermeko",
-    packages=['ft_package'],
-
-)
