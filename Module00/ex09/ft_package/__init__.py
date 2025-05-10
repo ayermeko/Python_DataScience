b724b4f340d07f17ae8bab8d09ba62550576e52b
Alibi <yoooholly71@gmail.com>
2025-05-10 15:46:46 +0200

    push

--- Module00/ex09/ft_package/__init__.py
+++ Module00/ex09/ft_package/__init__.py
@@ -1,5 +1,5 @@
 from .functions import count_in_list, check_for_special_characters
 
 
-count_in_list = count_in_list()
-check_for_special_characters = check_for_special_characters()
+count_in_list(range(5), 0)
+check_for_special_characters("hello")
