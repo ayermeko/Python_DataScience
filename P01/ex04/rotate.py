bc0f1349bab6de2c8bfeec65b6e18c182f28a1b7
Alibi <yoooholly71@gmail.com>
2025-06-20 15:43:31 +0200

    ex04 done might have some bugs TODO: fixing

--- P01/ex04/rotate.py
+++ P01/ex04/rotate.py
@@ -17,19 +17,39 @@ def zoom(image: np.array):
         return None
     return zoomed_image
 
+
+def rotate(img: np.array) -> np.array:
+    """
+    Transpose an image by a given numpy array.
+    """
+    try:
+        arrTraspose = np.copy(img[:, :, 0])
+
+        for y, row in enumerate(img):
+            for x, col in enumerate(row):
+                arrTraspose[x, y] = img[y, x][0]
+    except Exception as e:
+        print(f"{type(e).__name__}: {e}")
+    return arrTraspose
+
 def main():
     """
     This a main function to call a zoom function to see if it works.
     """
     img_array = ft_load("animal.jpeg")
-    print(img_array)
+    # print(img_array)
     zoomed_img = zoom(img_array)
     print(zoomed_img)
+    rotated_img = rotate(zoomed_img)
+    print(f"New shape after Transpose: {np.shape(rotated_img)}")
+    print(rotated_img)
+
     try:
-        plt.imshow(zoomed_img, cmap='gray')
+        plt.imshow(rotated_img, cmap='gray')
         plt.show()
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
+
 
 if __name__ == "__main__":
     main()
