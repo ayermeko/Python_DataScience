1a121f54d024e4a662f58cab167d658b5538d98d
Alibi <yoooholly71@gmail.com>
2025-06-20 13:02:03 +0200

    ex02 -> P01 completed

---
+++ P01/ex02/tester.py
@@ -0,0 +1,11 @@
+from load_image import ft_load
+
+
+def main():
+    """
+    The main program to run the loading function.
+    """
+    print(ft_load("landscape.jpg"))
+
+if __name__ == "__main__":
+    main()
