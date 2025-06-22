6bae87779e8304a0b5f81aad7a84e59ad1da7ed8
Alibi <yoooholly71@gmail.com>
2025-06-22 14:26:39 +0200

    new files to push

---
+++ P02/ex00/tester.py
@@ -0,0 +1,12 @@
+from load_csv import load
+
+
+def main():
+    """
+    The main function to test a fucntion form load_csv.py file
+    """
+    print(load("life_expectancy_years.csv"))
+
+
+if __name__ == "__main__":
+    main()
