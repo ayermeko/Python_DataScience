a4fba71449427f7e37c61d12c66ec1a8c9320c9a
Alibi <yoooholly71@gmail.com>
2025-06-22 15:06:38 +0200

    new files

---
+++ P02/ex01/aff_like.py
@@ -0,0 +1,16 @@
+from load_csv import load
+
+def ft_display():
+    """
+    This is a funciton to display a given parameters.
+    """
+    pass
+
+def main():
+    """
+    This function is a main to test a display.
+    """
+
+
+if __name__ == "__main__":
+    main()
