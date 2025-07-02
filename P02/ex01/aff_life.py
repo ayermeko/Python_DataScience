dd6c427d4b992d4a0d7b16e292a21e9748eae113
Alibi Yermekov <ayermeko@c2r5s1.42prague.com>
2025-07-02 19:09:25 +0200

    ex01 is done

--- P02/ex01/aff_life.py
+++ P02/ex01/aff_life.py
@@ -1,4 +1,4 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from load_csv import load
 
 
@@ -11,10 +11,22 @@ def main():
         if country not in dataset['country'].values:
             raise ValueError("Country is not in a dataset.")
         country_data = dataset[dataset['country'] == country]
-        
+        years = [int(year) for year in dataset.columns[1:] if str(year).isdigit()]
+        life_expectancy = country_data.iloc[0, 1:].values
+
+        plt.plot(years, life_expectancy, label=country)
+        plt.title(f"{country} Life expectancey Projections")
+        plt.xlabel("Year")
+        plt.ylabel("Life expectancy")
+        plt.legend()
+        plt.show()
     except Exception as e:
         print(f"{type(e).__name__}: {e}")
         return
+    except KeyboardInterrupt as e:
+        print(f"{type(e).__name__}: Program interrupted.")
+        return
+
 
 if __name__ == "__main__":
     main()
