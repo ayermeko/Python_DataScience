ccc58f71417f8af9056b50bcbdaa12e2a4f17f39
Alibi <yoooholly71@gmail.com>
2025-07-11 12:40:42 +0200

    puhs

--- P03/ex01/S1E7.py
+++ P03/ex01/S1E7.py
@@ -2,9 +2,11 @@ from S1E9 import Character
 
 
 class Baratheon(Character):
+    """Representing the Baratheon family."""
     pass
 
 
 class Lannister(Character):
+    """Representing the Lannister family."""
     def create_lannister():
         pass
